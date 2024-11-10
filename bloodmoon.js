05384bebc1f0faead10d1dafef3c975de32c95a2
zahra a.s <182934048+zhrbash@users.noreply.github.com>
2024-11-11 01:14:37 +0700

    WIP: Implement script for image filters
    - known issue: paralax jitter is worse on safari when filters are activated
    - known issue: button is hard to click on safari
    - known issue: script can only target one background image out of four

--- bloodmoon.js
+++ bloodmoon.js
@@ -3,19 +3,39 @@
 // simple script to activate the hidden "bloodmoon" theme 
 
 let isToggled = false; // initialize boolean to track toggle state
+let isFiltered = false;
 
+// event handler to toggle colors (white <-> red)
 document.getElementById('change-color').addEventListener('click', function() {
 
     if (!isToggled) {
-        // change color values of multiple CSS variables (toggle true)
+        // change color values of multiple CSS variables (isToggle = true)
         document.documentElement.style.setProperty('--foreground', 'rgb(197, 22, 22)'); // change main foreground color to red
         document.documentElement.style.setProperty('--accent-color', 'white'); // change accent color to white
     } else {
-        // change them back (toggle false)
+        // change them back (isToggle = false)
         document.documentElement.style.setProperty('--foreground', 'white'); // change main foreground color back to white
         document.documentElement.style.setProperty('--accent-color', 'rgb(197, 22, 22)'); // change accent color back to red
     }
 
-    isToggled = !isToggled; // toggle the state
+    isToggled = !isToggled; // toggle the color state
 
+});
+
+// event handler to toggle image filers 
+document.getElementById('change-color').addEventListener('click', function() {
+    const image = document.getElementById('filter-bm-image');
+    const backgroundBox = document.getElementById('filter-bm-background');
+
+    if (!isFiltered) {
+        // apply filter (isToggle = true)
+        image.style.filter = 'brightness(70%) contrast(125%) saturate(170%)'; // 
+        backgroundBox.style.filter = 'brightness(70%) saturate(170%)';
+    } else {
+        // remove filter (isToggle = false) 
+        image.style.filter = 'none'; // remove filter back to original state (which is none)
+        backgroundBox.style.filter = 'none';
+    }
+
+    isFiltered = !isFiltered
 });
